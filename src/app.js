e14174a2a9dd13cd343ea0aede740d5d051ae42b
Alba Nebot López <168462451+albanebotlopez@users.noreply.github.com>
2024-07-05 15:25:48 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function CartaAleatoria() {
+  let numerosCartas = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  return numerosCartas[Math.floor(Math.random() * numerosCartas.length)];
+}
+
+function PaloAleatorio() {
+  let Palos = ["♦️", "♥️", "♠️", "♣️"];
+  return Palos[Math.floor(Math.random() * Palos.length)];
+}
+
+function generarCartaAleatoria() {
+  let valor = CartaAleatoria();
+  let palo = PaloAleatorio();
+
+  document.getElementById("palo-superior-izquierda").innerHTML = palo;
+  document.getElementById("numero-centro").innerHTML = valor;
+  document.getElementById("palo-inferior-derecha").innerHTML = palo;
+
+  let color = palo === "♥️" || palo === "♦️" ? "red" : "black";
+  document.getElementById("palo-superior-izquierda").style.color = color;
+  document.getElementById("numero-centro").style.color = color;
+  document.getElementById("palo-inferior-derecha").style.color = color;
+}
+
+window.onload = generarCartaAleatoria;
